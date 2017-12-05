da65bf9d169ccdc4fd564ec4e2e3eb3db3b5231d
Artemas <artemasseng@gmail.com>
2017-12-05 23:32:32 +0000

    add static files

--- library/app.js
+++ library/app.js
@@ -4,6 +4,20 @@ var app = express(); // This gives us an instance of express that we can do stuf
 var port = 5000; // This is the port that Express listens on.
 
 /**
+ * app.use() - Use third-party middleware to add functionality to Express apps.
+ */
+app.use(express.static('public')); // Serves static resources in the `public` directory
+app.use(express.static('src/views')); // Serves static resources in the `src/views` directory
+
+/**
+ * app.get() will allow routing by allowing requests to be able to route
+ * to a desired location on the application and get a response.
+ */
+app.get("/", function(req, res){
+    res.send('Hello World');
+});
+
+/**
  * function(err) is a callback.
  * Callback - a function that is executed by another function
  * in our case app.listen() will execute function(err) once it
